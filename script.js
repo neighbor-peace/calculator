201a79f89b2f6cdd9bce334e1faa6e902f80c0bc
Michael Costello <mcostello91@gmail.com>
2022-05-22 15:18:45 -0700

    Clean up code and add clarifying comments

--- script.js
+++ script.js
@@ -1,73 +1,46 @@
-function add(x, y) {
-    return parseFloat(x) + parseFloat(y);
-};
-
-function subtract(x, y) {
-    return x - y;
-};
-
-function multiply(x, y) {
-    return x * y;
-};
-
-function divide(x, y) {
-    return x / y;
-};
-
-function operate(operator, x, y) {
-    let result;
-    switch(operator) {
-        case '+':
-            result = add(x, y);
-            break;
-        case '-': 
-            result = subtract(x, y);
-            break;
-        case 'x':
-            result = multiply(x, y);
-            break;
-        case '÷':
-            result = divide(x, y);
-    };
-    if (result = Infinity) {
-        return "Nice Try";
-    } else {
-        return Number.isInteger(result) ? result : result.toFixed(3);
-    };
-};
-
-//use object for holding input until user presses "="
-let inputObj = {};
-
-//put event listener on each button for inputHandler
+const display = document.querySelector('#display');
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', inputHandler);
 });
 
-const display = document.querySelector('#display');
+let inputObj = {};
 
-function AC() {
-    inputObj = {};
-    display.textContent = '';
-}
+function inputHandler() {
+    let input = this.textContent;
+    if (!isNaN(input)) {
+        numbers(input);
+    } else if (input === 'AC') {
+        AC();
+    } else if (input === '=') {
+        equals();
+    } else {
+        operator(input);
+    };
+};
 
 function numbers(input) {
     if (!inputObj.operator) {
+        //inputObj.calculated stops new input from concatenating to previous result
         if (!inputObj.operandA || inputObj.calculated) {
             inputObj.operandA = input;
         } else {
             inputObj.operandA += input;
-        }
+        };
         display.textContent = inputObj.operandA;
     } else if (inputObj.operator) {
         if (!inputObj.operandB) {
             inputObj.operandB = input;
         } else {
             inputObj.operandB += input;
-        }
+        };
         display.textContent = inputObj.operandB;
     };
+};
+
+function AC() {
+    inputObj = {};
+    display.textContent = '';
 };
 
 function equals() {
@@ -77,7 +50,8 @@ function equals() {
         let result = operate(inputObj.operator, inputObj.operandA, inputObj.operandB);
         inputObj = {
             operandA: result,
-            calculated: true,
+            //"calculated: true" stops new input from concatenating to previous result
+            calculated: true, 
         };
         display.textContent = result;
     };
@@ -99,17 +73,38 @@ function operator(input) {
     };
 };
 
-function inputHandler() {
-    let input = this.textContent;
-    //numbers
-    if (!isNaN(parseFloat(input))) {
-        numbers(input);
-    } else if (input === 'AC') {
-        AC();
-    } else if (input === '=') {
-        equals();
+function operate(operator, x, y) {
+    let result;
+    switch(operator) {
+        case '+':
+            result = add(x, y);
+            break;
+        case '-': 
+            result = subtract(x, y);
+            break;
+        case 'x':
+            result = multiply(x, y);
+            break;
+        case '÷':
+            result = divide(x, y);
+    };
+    if (result === Infinity) {
+        return "Nice Try";
     } else {
-        operator(input);
+        return Number.isInteger(result) ? result : result.toFixed(3);
     };
 };
 
+
+function add(x, y) {
+    return parseFloat(x) + parseFloat(y);
+};
+function subtract(x, y) {
+    return x - y;
+};
+function multiply(x, y) {
+    return x * y;
+};
+function divide(x, y) {
+    return x / y;
+};
